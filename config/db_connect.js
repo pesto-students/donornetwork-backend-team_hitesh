be66383019b29b25f8361cc5a7a3d239d4077b00
hamrosh <hamroshroy@gmail.com>
2023-03-24 14:40:15 +0530

    db check

--- config/db_connect.js
+++ config/db_connect.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql'; 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ham242k24MYSQL$",
+    host: "db-mysql-blr1-45948-do-user-13818185-0.b.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "AVNS_E3Fr5wD6vYOoJwWp2l9$",
     database:"donor_network"
 });
   
