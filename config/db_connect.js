1bcf091018995cb72cfaf4d8904a96250008fb9f
hamrosh <hamroshroy@gmail.com>
2023-03-25 09:48:14 +0530

    env changes

--- config/db_connect.js
+++ config/db_connect.js
@@ -1,11 +1,15 @@
 import mysql from 'mysql'; 
+import * as dotenv from 'dotenv';
+import path from 'path';
+import methodOverride from 'method-override';
+dotenv.config();
+console.log(process.env.DB_HOST,process.env.DB_PASSWORD)
 const db = mysql.createConnection({
-    port:25060,
-    host: "db-mysql-blr1-45948-do-user-13818185-0.b.db.ondigitalocean.com",
-    user: "doadmin",
-    password: "AVNS_E3Fr5wD6vYOoJwWp2l9",
-    database:"donor_network",
-  
+    port:process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 });
   
 db.connect(function(err) {
